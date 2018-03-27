624f1ac40ebbab74d5e994839bd46c62fbf70c8d
Nicholas Meyers <s171681@students.aie.edu.au>
2018-03-27 15:55:10 +1030

    Initial commit

---
+++ Assets/CharacterStats.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+
+public class CharacterStats : MonoBehaviour {
+    
+    public enum ExternalName {
+        HEALTH,
+        STAMINA,
+        MANA,
+        ENAMES
+    };
+
+    public enum InternalName {
+        DAMAGE,
+        DEFENCE,
+        INAMES
+    }
+    
+    // the external stats of the characer (eg health, stamina)
+    [System.Serializable]
+    public struct ExternalStat {
+        public ExternalName characterStat;
+        public float current;
+        public float maximum;
+    }
+
+    // the internal stats of the character (eg strength, intelligence)
+    [System.Serializable]
+    public struct InternalStat {
+        public InternalName characterStat;
+        public float current;
+    }
+
+    public ExternalStat[] m_externalStats;
+    public InternalStat[] m_internalStats;
+    
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.A)) {
+            Damage(ExternalName.HEALTH, 10);
+        }
+	}
+
+    float CalculateDamage(float damage) {
+        // Place any stats for calculating damage here
+        InternalStat internalStat1 = GetIntStat(InternalName.DEFENCE);
+
+        // This is where the damage calculation will take place
+        damage = damage - internalStat1.current;
+        
+        // to avoid healing the target with an attack, do not delete this
+        if (damage > 0) {
+            return damage;
+        } else {
+            return 0;
+        }
+    }
+
+    void Damage(ExternalName listIndex, float damage) {
+        ExternalStat extTemp = GetExtStat(listIndex);
+
+        extTemp.current = extTemp.current - CalculateDamage(damage);
+        
+        m_externalStats[(int)ExternalName.HEALTH] = extTemp;
+    } 
+
+    public ExternalStat GetExtStat(ExternalName name) {
+        return m_externalStats[(int)name];
+    }
+    InternalStat GetIntStat(InternalName name) {
+        return m_internalStats[(int)name];
+    }
+}
---
+++ Assets/StatCard.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StatCard : MonoBehaviour {
+
+    public CharacterStats characterStats;
+    
+    public CharacterStats.ExternalName affectedStat;
+
+    private float currentValue;
+    private float maxValue;
+    private Image meter;
+
+	// Use this for initialization
+	void Start () {
+        meter = gameObject.GetComponent<Image>();
+        currentValue = characterStats.GetExtStat(affectedStat).current;
+        maxValue = characterStats.GetExtStat(affectedStat).maximum;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        currentValue = characterStats.GetExtStat(affectedStat).current;
+        maxValue = characterStats.GetExtStat(affectedStat).maximum;
+        meter.fillAmount = currentValue / maxValue;
+	}
+}
